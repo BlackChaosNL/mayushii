22d6038dbf923c225dc481ef0300d5bc4313488e
BlackChaosNL <jjvijgen@gmail.com>
2015-09-28 09:50:36 +0000

    Refactor of code.
    Changed Constructor, added CreateConnection.
    WIP

--- irc.java
+++ irc.java
@@ -1,19 +1,21 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 public class irc {
-	
-	/**
-	 * Default 
-	 */
+
 	private String server;
+	private int port;
 	private String username;
 	private String login;
 	private String channel;
 	private Socket socket;
 	private BufferedWriter bufferedWriter;
 	private BufferedReader bufferedReader;
-	
+
 	/**
 	 * @return the server
 	 */
@@ -22,10 +24,26 @@ public class irc {
 	}
 
 	/**
-	 * @param server the server to set
+	 * @param server
+	 *            the server to set
 	 */
 	public void setServer(String server) {
 		this.server = server;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @param port
+	 *            the port to set
+	 */
+	public void setPort(int port) {
+		this.port = port;
 	}
 
 	/**
@@ -36,7 +54,8 @@ public class irc {
 	}
 
 	/**
-	 * @param username the username to set
+	 * @param username
+	 *            the username to set
 	 */
 	public void setUsername(String username) {
 		this.username = username;
@@ -50,7 +69,8 @@ public class irc {
 	}
 
 	/**
-	 * @param login the login to set
+	 * @param login
+	 *            the login to set
 	 */
 	public void setLogin(String login) {
 		this.login = login;
@@ -64,7 +84,8 @@ public class irc {
 	}
 
 	/**
-	 * @param channel the channel to set
+	 * @param channel
+	 *            the channel to set
 	 */
 	public void setChannel(String channel) {
 		this.channel = channel;
@@ -78,7 +99,8 @@ public class irc {
 	}
 
 	/**
-	 * @param socket the socket to set
+	 * @param socket
+	 *            the socket to set
 	 */
 	public void setSocket(Socket socket) {
 		this.socket = socket;
@@ -92,7 +114,8 @@ public class irc {
 	}
 
 	/**
-	 * @param bufferedWriter the bufferedWriter to set
+	 * @param bufferedWriter
+	 *            the bufferedWriter to set
 	 */
 	public void setBufferedWriter(BufferedWriter bufferedWriter) {
 		this.bufferedWriter = bufferedWriter;
@@ -106,32 +129,47 @@ public class irc {
 	}
 
 	/**
-	 * @param bufferedReader the bufferedReader to set
+	 * @param bufferedReader
+	 *            the bufferedReader to set
 	 */
 	public void setBufferedReader(BufferedReader bufferedReader) {
 		this.bufferedReader = bufferedReader;
 	}
 
 	/**
-	 * Connect to IRC then login.
+	 * Construct the class.
+	 * 
 	 * @param server
 	 * @param port
 	 * @param username
 	 * @param login
 	 * @param channel
-	 * @throws UnknownHostException
-	 * @throws IOException
 	 */
-	public irc(String server, int port, String username, String login, String channel){
-			try {	this.setSocket(new Socket(server, port));
-					this.setBufferedWriter(new BufferedWriter(new OutputStreamWriter(this.getSocket().getOutputStream())));
-					this.setBufferedReader(new BufferedReader(new InputStreamReader(this.getSocket().getInputStream())));
-					this.useIrc(username, login, channel, this.getBufferedWriter(), this.getBufferedReader()); 
-				} catch (IOException e) { System.err.println("IrcClient: Something went wrong here!?!");}
+	public irc(String server, int port, String username, String login, String channel) {
+		this.setServer(server);
+		this.setPort(port);
+		this.setUsername(username);
+		this.setLogin(login);
+		this.setChannel(channel);
 	}
-	
+
+	/**
+	 * Creates the connection to the designated IRC
+	 */
+	public void CreateConnection() {
+		try {
+			this.setSocket(new Socket(this.getServer(), this.getPort()));
+			this.setBufferedWriter(new BufferedWriter(new OutputStreamWriter(this.getSocket().getOutputStream())));
+			this.setBufferedReader(new BufferedReader(new InputStreamReader(this.getSocket().getInputStream())));
+			this.useIrc(username, login, channel, this.getBufferedWriter(), this.getBufferedReader());
+		} catch (IOException e) {
+			System.err.println("IrcClient: Something went wrong here!?!");
+		}
+	}
+
 	/**
 	 * Login+keep alive to the IRC service.
+	 * 
 	 * @param nick
 	 * @param login
 	 * @param channel
@@ -139,29 +177,29 @@ public class irc {
 	 * @param bufferedReader
 	 * @throws IOException
 	 */
-	private void useIrc(String nick, String login, String channel, BufferedWriter bufferedWriter, BufferedReader bufferedReader) throws IOException{
-		int joined = 0;
+	private void useIrc(String nick, String login, String channel, BufferedWriter bufferedWriter,
+			BufferedReader bufferedReader) throws IOException {
 		int ping = 0;
-		bufferedWriter.write("USER "+login+" foo foo foo :"+login+"\r\n");
+		int joined = 0;
+		String line = null;
+		bufferedWriter.write("USER " + login + " foo foo foo :" + login + "\r\n");
 		bufferedWriter.write("NICK " + nick + "\r\n");
 		bufferedWriter.flush();
-		String line = null;
-		while((line = bufferedReader.readLine()) != null){
-			System.out.println(line);
-			if(line.matches("(.*)PING(.*)")){
+		while ((line = bufferedReader.readLine()) != null) {
+			if (line.matches("(.*)PING(.*)")) {
 				bufferedWriter.write("PONG " + line.substring(5) + "\r\n");
 				bufferedWriter.flush();
-                ping++;
-			}else{
-				if(joined == 0 && ping >= 2){
+				ping++;
+			} else {
+				if (joined == 0 && ping >= 2) {
 					String[] channels = channel.split(",", -1);
-					for(int i = 0; i < channels.length; i++){
-						bufferedWriter.write("JOIN "+ channels[i] + "\r\n");
+					for (int i = 0; i < channels.length; i++) {
+						bufferedWriter.write("JOIN " + channels[i] + "\r\n");
 					}
 					bufferedWriter.flush();
-			        joined = 1;
+					joined = 1;
 				}
 			}
-        }
+		}
 	}
 }
